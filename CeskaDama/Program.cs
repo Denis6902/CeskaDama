f99defc4c82f23948a1d28193e0f0070b693e274
Denis6902 <62514727+Denis6902@users.noreply.github.com>
2023-11-14 16:13:53 +0100

    Přídaní odkazu na návod

--- CeskaDama/Program.cs
+++ CeskaDama/Program.cs
@@ -1,6 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 
 using CeskaDama;
+
+// http://www.deskovehry.info/pravidla/ceska-dama.htm
 
 HraCeskaDama ceskaDama = new HraCeskaDama();
 ceskaDama.ZacitHru();
