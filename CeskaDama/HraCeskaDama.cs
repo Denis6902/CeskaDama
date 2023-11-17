b39608acc28a2833e803f6ae33f43e2bfef5b5e8
Denis6902 <bata.d@email.cz>
2023-11-17 11:42:16 +0100

    Vetsi gap pri vypisu

--- CeskaDama/HraCeskaDama.cs
+++ CeskaDama/HraCeskaDama.cs
@@ -334,12 +334,13 @@ public class HraCeskaDama
     private void VypisHerniDesku(bool barevne = false, int barevneX = -1, int barevneY = -1)
     {
         Console.Clear();
-        Console.Write("    Y Y Y Y Y Y Y Y\n");
-        Console.Write("    0 1 2 3 4 5 6 7\n");
+        Console.Write("     Y Y Y Y Y Y Y Y \n");
+        Console.Write("     0 1 2 3 4 5 6 7 \n");
+        Console.Write("                     \n");
 
         for (int x = 0; x < HerniDeska.GetLength(0); x++)
         {
-            Console.Write($"X {x} ");
+            Console.Write($"X {x}  ");
             for (int y = 0; y < HerniDeska.GetLength(1); y++)
             {
                 if (barevne && x == barevneX && y == barevneY)
